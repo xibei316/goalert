2dd426f6ed6d547f80bc74a91600167dbb57048c
Nathaniel Caza <mastercactapus@gmail.com>
2021-06-23 16:20:33 -0500

    add persistence hook

--- web/src/app/schedules/ScheduleDeleteDialog.js
+++ web/src/app/schedules/ScheduleDeleteDialog.js
@@ -20,15 +20,25 @@ const mutation = gql`
   }
 `
 
+function usePersistentData(fn, ...deps) {
+  const [value, setValue] = useState(fn())
+  useEffect(() => {
+    const newValue = fn()
+    if (!newValue) return
+
+    setValue(newValue)
+  }, deps)
+
+  return value
+}
+
 export default function ScheduleDeleteDialog(props) {
   const { data, loading: dataLoading } = useQuery(query, {
     onClose: p.func,
     variables: { id: props.scheduleID },
   })
-  const [name, setName] = useState(get(data, 'schedule.name'))
-  useEffect(() => {
-    if (data?.schedule?.name) setName(data?.schedule?.name)
-  }, [data?.schedule?.name])
+
+  const name = usePersistentData(() => data?.schedule?.name, [data])
 
   const [deleteSchedule, deleteScheduleStatus] = useMutation(mutation, {
     onCompleted: props.onClose,
