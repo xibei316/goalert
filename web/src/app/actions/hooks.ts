f6d43b3ca526bd7028bb3df70fd91cd66a2d7d82
dctalbot <david.talbot@target.com>
2021-12-14 16:11:11 -0600

    use react-router hooks

--- web/src/app/actions/hooks.ts
+++ web/src/app/actions/hooks.ts
@@ -1,9 +1,10 @@
 import { urlParamSelector, urlPathSelector } from '../selectors'
-import { setURLParam as setParam, resetURLParams as resetParams } from './main'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { warn } from '../util/debug'
 import joinURL from '../util/joinURL'
 import { pathPrefix } from '../env'
+import { useHistory, useLocation } from 'react-router'
+import { sanitizeParam } from './main'
 
 export type Value = string | boolean | number | string[]
 
@@ -11,10 +12,11 @@ export function useURLParam<T extends Value>(
   name: string,
   defaultValue: T,
 ): [T, (newValue: T) => void] {
-  const dispatch = useDispatch()
   const urlParam = useSelector(urlParamSelector)
   const urlPath = joinURL(pathPrefix, useSelector(urlPathSelector))
   const value = urlParam(name, defaultValue) as T
+  const location = useLocation()
+  const history = useHistory()
 
   function setValue(newValue: T): void {
     if (window.location.pathname !== urlPath) {
@@ -23,15 +25,38 @@ export function useURLParam<T extends Value>(
       )
       return
     }
-    dispatch(setParam(name, newValue, defaultValue))
+
+    const value = name === 'search' ? newValue : sanitizeParam(newValue)
+
+    const q = new URLSearchParams(location.search)
+    if (Array.isArray(value)) {
+      q.delete(name)
+      value.forEach((v) => q.append(name, v))
+    } else if (value) {
+      q.set(name, value)
+    } else {
+      q.delete(name)
+    }
+
+    if (q.sort) q.sort()
+    let newSearch = q.toString()
+    newSearch = newSearch ? '?' + newSearch : ''
+
+    if (newSearch === location.search) {
+      // no action for no param change
+      return
+    }
+    history.replace(location.pathname + newSearch + location.hash)
   }
 
   return [value, setValue]
 }
 
 export function useResetURLParams(...keys: Array<string>): () => void {
-  const dispatch = useDispatch()
   const urlPath = joinURL(pathPrefix, useSelector(urlPathSelector))
+  const location = useLocation()
+  const history = useHistory()
+
   function resetURLParams(): void {
     if (window.location.pathname !== urlPath) {
       warn(
@@ -39,7 +64,23 @@ export function useResetURLParams(...keys: Array<string>): () => void {
       )
       return
     }
-    dispatch(resetParams(...keys))
+
+    if (!keys.length) return history.replace(location.pathname)
+
+    const q = new URLSearchParams(location.search)
+    keys.forEach((key) => {
+      q.delete(key)
+    })
+
+    if (q.sort) q.sort()
+    let newSearch = q.toString()
+    newSearch = newSearch ? '?' + newSearch : ''
+
+    if (newSearch === location.search) {
+      // no action for no param change
+      return
+    }
+    history.replace(location.pathname + newSearch + location.hash)
   }
 
   return resetURLParams
