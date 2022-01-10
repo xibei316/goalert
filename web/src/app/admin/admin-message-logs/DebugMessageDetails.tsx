67e22cda0cca7f952582a14a53820469e0c977bb
dctalbot <david.talbot@target.com>
2022-01-10 11:22:12 -0600

    extract helper

--- web/src/app/admin/admin-message-logs/DebugMessageDetails.tsx
+++ web/src/app/admin/admin-message-logs/DebugMessageDetails.tsx
@@ -15,6 +15,7 @@ import AppLink from '../../util/AppLink'
 import { DebugMessage } from '../../../schema'
 import makeStyles from '@mui/styles/makeStyles/makeStyles'
 import { theme } from '../../mui'
+import { getOpenInNewURL } from '../../styles/getOpenInNewURL'
 
 interface Props {
   open: boolean
@@ -24,9 +25,7 @@ interface Props {
 
 const useStyles = makeStyles<typeof theme>((theme) => ({
   newTab: {
-    backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath xmlns='http://www.w3.org/2000/svg' d='M18 19H6c-.55 0-1-.45-1-1V6c0-.55.45-1 1-1h5c.55 0 1-.45 1-1s-.45-1-1-1H5a2 2 0 0 0-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-6c0-.55-.45-1-1-1s-1 .45-1 1v5c0 .55-.45 1-1 1zM14 4c0 .55.45 1 1 1h2.59l-9.13 9.13a.996.996 0 1 0 1.41 1.41L19 6.41V9c0 .55.45 1 1 1s1-.45 1-1V4c0-.55-.45-1-1-1h-5c-.55 0-1 .45-1 1z' fill='${encodeURIComponent(
-      theme.palette.anchor.main,
-    )}' /%3E%3C/svg%3E");`,
+    backgroundImage: getOpenInNewURL(theme.palette.anchor.main),
     backgroundPosition: 'center right',
     backgroundRepeat: 'no-repeat',
     paddingRight: '17px',
